adecf0ffca08e1302c1c5c3dabbe238ffa878ed9
kouga99 <167828897+kouga99@users.noreply.github.com>
2024-08-18 10:38:31 -0300

    primeiros códigos/funções/loops

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 alert ('Bem vindo ao Adivinha Aí!');
 alert('Você tem 10 tentativas para acertar o número misterioso');
 
-let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
-let chute = prompt('Chute seu número entre 1 e 100');
+//Função para gerar um numero aleatório
+function gerarNumeroAleatorio() {
+    return Math.floor(Math.random() * 100) + 1;
+}
 
-//Se o chute for igual ao número misterioso
-if (numeroAleatorio == chute){
-    alert(`Isso ai! Você acertou! O número misterioso é ${numeroAleatorio}`);
-} else {
-    alert(`Você errou! Tente novamente!`)
+//Função principal do jogo
+function jogar (){
+    let chute 
+    const tentativasMax = 10;
+    let tentativas = 0;
+    const numeroAleatorio = gerarNumeroAleatorio();
+    let acertou = false;
+}
+//Enquanto o chute não for igual ao número ou acabar as chances
+while(chute != numeroAleatorio) {
+    chute = parseInt(prompt('Digite seu palpite (entre 1 e 100):'), 10);
+    //Se o chute for igual ao número misterioso
+    if (chute == numeroAleatorio) {
+        alert(`Isso ai! Você acertou! O número misterioso é ${numeroAleatorio}`);
+    } else {
+        if (chute > numeroAleatorio) {
+            alert(`${chute} é muito alto sio!`)
+        } else {
+            alert(`${chute} é muito baixo sio!`)
+        }
+    }
+
 }
